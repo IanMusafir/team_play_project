5199e4b4823a3921bcd078d907ee9be7fd12446d
Abdulrahman <rahman3000@mail.ru>
2023-09-10 21:58:03 +0300

    сделал модель  юзера

--- models/User.model.js
+++ models/User.model.js
@@ -1,1 +1,12 @@
-//здесь будет схема-модель для пользователей 
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  nickName: String,
+  login: String,
+  password: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
+//здесь будет схема-модель для пользователей
