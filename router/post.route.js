2fad30214fd4cf12906ed30e5a3385d5ab279b30
Abuyusuf99 <mus.korol@mail.ru>
2023-09-12 13:20:33 +0300

    сделал коммит перед апстримом

--- router/post.route.js
+++ router/post.route.js
@@ -1,26 +1,21 @@
-<<<<<<< HEAD
 
 //тут роуты для постов
 
-=======
->>>>>>> a9aeac8d486883b1234b1c7e61c46cde3b887b67
+
 const {Router} = require("express")
 const {postControllers} = require("../controller/post.controller")
 
 const router = Router()
 
 
-router.post('/Posts', postControllers.createPost)
-router.get('/Posts', postControllers.getPost)
-router.get('/Posts/:id', postControllers.getOnePost)
-router.delete('/Posts', postControllers.deletePost)
-<<<<<<< HEAD
-router.patch('/Posts/:id', postControllers.patchPost)
+router.post('/posts', postControllers.createPost)
+router.get('/posts', postControllers.getPost)
+router.get('/posts/:id', postControllers.getOnePost)
+router.delete('/posts', postControllers.deletePost)
+router.patch('/posts/:id', postControllers.patchPost)
 
 module.exports = router
 
-=======
-router.patch('/Posts', postControllers.patchPost)
 
-module.exports = router
->>>>>>> a9aeac8d486883b1234b1c7e61c46cde3b887b67
+
+
